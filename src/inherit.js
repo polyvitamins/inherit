2e5ba651da05cc25c4cde0f97fc44766e29bab57
Vladimir Morulus <vladimirmorulus@gmail.com>
2015-12-20 19:07:54 +0300

    global scope

--- src/inherit.js
+++ src/inherit.js
@@ -86,7 +86,7 @@ var mixin = require("mixin");
 			Мы выполняем расширенные функции только если мы являемся экземпляром Mixin
 			*/			
 			
-			if (!(this===window)) {
+			if (!(this===window||this===global)) {
 				superconstructor.apply(this, args)
 			}
 
